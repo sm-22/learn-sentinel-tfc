e8f689e62f0682aa096b3d48b1db0c28c2e23bc5
sm-22 <113645529+sm-22@users.noreply.github.com>
2022-09-25 15:44:13 -0400

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "<YOUR_TERRAFORM_ORG>"
+    organization = "aws829592"
 
     workspaces {
       name = "sentinel-example"
